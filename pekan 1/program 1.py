e691c74c452f85b8725f5d0c73c52cf5429a1a1e
agungfirmansyah0623 <agungfirmansyah0623@gmail.com>
2026-09-01 10:18:51 +0700

    praktikum 1

---
+++ pekan 1/program 1.py
@@ -0,0 +1,13 @@
+# Program Pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("hello world")
+print()
+print("program ini menghasilkan")
+print("empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a/n" 
++"Agunng firmansyah 2611533011")
