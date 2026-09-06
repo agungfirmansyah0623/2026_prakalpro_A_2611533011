fa9a74a62f75dae1a26295855f742c797a512c87
agungfirmansyah0623 <agungfirmansyah0623@gmail.com>
2026-09-06 15:58:34 +0700

    bisa ni

--- pekan 1/program 1.py
+++ pekan 1/program 1.py
@@ -10,4 +10,4 @@ print("program ini menghasilkan")
 print("empat baris output")
 print("/ \\ // \\\\ /// \\\\\\")
 print("program ini" + "dibuat oleh a/n" 
-+"Agunng firmansyah 2611533011")
++"Agung Firmansyah 2611533011")
