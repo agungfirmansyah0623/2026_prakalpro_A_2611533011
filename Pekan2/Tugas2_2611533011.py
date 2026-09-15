3fd1cea6430182138a0c4d8115b9b43d68d99a2d
agungfirmansyah0623 <agungfirmansyah0623@gmail.com>
2026-09-15 20:02:50 +0700

    2026_PrakAlpro_[IF26_A]_[2611533011])

--- Pekan2/Tugas2_2611533011.py
+++ Pekan2/Tugas2_2611533011.py
@@ -13,7 +13,6 @@ jenis_kelamin_3011 = input("Masukkan Jenis Kelamin (L/P): ")
 umur_3011 = int(input("Masukkan Umur : "))
 skor_tes_3011 = float(input("Masukkan Skor Tes Awal : "))
 
-
 # Data String
 alamat_3011 = """Jl. Kampus Unand,
 Kecamatan Kuranji,
