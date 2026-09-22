90bcb493da9d18ba715501d81816b1003b57d7aa
agungfirmansyah0623 <agungfirmansyah0623@gmail.com>
2026-09-22 08:57:58 +0700

    oke

--- pekan4/if2_2611533011.py
+++ pekan4/if2_2611533011.py
@@ -1,7 +1,7 @@
 # Buat program untuk kondisional if
 # Program ini menggunakan fungsi input()
 
-ipk_3011 = (input("Input IPK Anda = "))
+ipk_3011 = float(input("Input IPK Anda = "))
 if ipk_3011 > 2.75:
     print("Anda lulus sangat memuaskan dengan IPK" + str(ipk_3011))
 else:
