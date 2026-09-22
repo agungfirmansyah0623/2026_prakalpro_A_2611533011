ff20a508ae4c905be584074e0b08da96bdcc9268
agungfirmansyah0623 <agungfirmansyah0623@gmail.com>
2026-09-22 09:46:26 +0700

    last

---
+++ pekan4/multi_if2_2611533011.py
@@ -0,0 +1,41 @@
+# Buat program untuk kondisional if
+# Program ini menggunakan fungsi input()
+# Progaram menghitung diskon belanja
+
+# input dari user
+total_belanja_3011 = float(input("Masukkan Total Belanja (Rp):"))
+
+# input status member (mengecek apakah user mengetik "y" atau "ya")
+input_member_3011 = input("Apakah anda member? (y/t)").strip().lower()
+is_member_3011 = input_member_3011 in ["y", "ya"]
+
+# input status kode promo 
+input_promo_3011 = input("Apakah kode promo valid? (y/t): ").strip().lower()
+kode_promo_3011 = input_promo_3011 in ["y", "ya"]
+
+total_diskon_persen_3011 = 0
+
+# multi-if terpisah
+#
+
+if total_belanja_3011 > 1000000:
+    total_diskon_persen_3011 += 10 #diskon belanja besar
+
+if is_member_3011:
+    total_diskon_persen_3011 += 5 #diskon member
+
+if kode_promo_3011:
+    total_diskon_persen_3011 += 15 #diskon promo
+
+#menghitung nominal diskon dan total bayar
+nominal_diskon_3011 = total_belanja_3011 * (total_diskon_persen_3011 / 100)
+total_bayar_3011 = total_belanja_3011-nominal_diskon_3011
+
+#output hasil
+print("\n--- Rincia  Pembayaran---")
+print(f"Total Diskon : {total_diskon_persen_3011}% (Rp{nominal_diskon_3011:,.0f})")
+print(f"otal_bayar_3011: Rp {total_bayar_3011:,.0f}")
+
+print(f"Total diskon yang anda dapatkan: {total_diskon_persen_3011}%")
+
+#Output: total diskon yang anda dapatkan: 30% jika belanja > 1 juta, member, dan kode promo valid
---
+++ pekan4/nilai_2611533011.py
@@ -0,0 +1,15 @@
+# buat program untuk nested if
+#fungsi input()
+# program ini konversi nilai angka menjadi huruf
+
+nilai_3011 = int(input("Inputkan nilai angka= "))
+
+if nilai_3011 >= 81:
+    print("A")
+elif nilai_3011 >= 70:
+    print("B")
+elif nilai_3011 >= 60:
+    print("C")
+elif nilai_3011 >= 50:
+    print("D")
+    
